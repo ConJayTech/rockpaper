d5c80c17b6cd4b0f12d8f260ee193fb2bb576b66
Conor Teichroeb <conor.teich@gmail.com>
2023-10-13 16:28:42 -0700

    adds 5 round game with score tracker

--- rockpaper.js
+++ rockpaper.js
@@ -1,5 +1,5 @@
     // Getting User Input
-    function playerSelection () {
+    function playerSelection() {
         const PlayerChoice = prompt("Choose rock, paper, or scissors").toLocaleLowerCase();
         if(PlayerChoice ==="paper" || PlayerChoice ==="rock" || PlayerChoice ==="scissors"){
             return PlayerChoice;
@@ -8,7 +8,7 @@
         } }
         
     // Getting Computer Input
-    function computerSelection () {
+    function computerSelection() {
         return choice = ['rock', 'paper', 'scissors'][Math.floor(Math.random()*3)];
     }      
     
@@ -21,11 +21,43 @@
             (computerSelection == "rock" && playerSelection == "scissors") ||
             (computerSelection == "scissors" && playerSelection == "paper") ||
             (computerSelection == "paper" && playerSelection == "rock")) {
-            return "Computer Wins!";
+            return "You Lose!";
+
         } else if (
             (computerSelection == "rock" && playerSelection == "paper") ||
             (computerSelection == "scissors" && playerSelection == "rock") ||
             (computerSelection == "paper" && playerSelection == "scissors")) {
-            return "Player Wins!";
+            return "You Win!";
         }
     }
+   
+    //Playing a Game of 5 Rounds
+    function game() {
+        let computerScore = 0;
+        let playerScore = 0;
+
+        for (i=0;i<5;i++) {
+
+            const result = playRound(playerSelection(), computerSelection());
+            console.log(result);
+            if (result.includes("Win!")) {
+                playerScore++;
+                console.log("Computer:" + computerScore + "|" + "Player:" + playerScore);
+            } else if (result.includes("Lose!")) {
+                computerScore++;
+                console.log("Computer:" + computerScore + "|" + "Player:" + playerScore);
+            }
+        }
+        console.log(
+            "Final Results: Player: " + playerScore + " | " + "Computer: " + computerScore
+        );
+        if (playerScore>computerScore) {
+            console.log("You Win the Game!");
+        } else if (playerScore<computerScore) {
+            console.log("You Lose the Game!");
+        } else {
+            console.log("The Game was a tie!");
+        }
+    }
+   
+game();
