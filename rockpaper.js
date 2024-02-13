8813b1a16b0203d3cf5efdf6cf781e61e3689d63
Conor Teichroeb <conor.teich@gmail.com>
2024-02-12 17:35:41 -0800

    Adds event listeners to buttons, cooperates with playRound function, console.logs winner

--- rockpaper.js
+++ rockpaper.js
@@ -1,11 +1,11 @@
     // Getting User Input
-    function playerSelection() {
-        const PlayerChoice = prompt("Choose rock, paper, or scissors").toLocaleLowerCase();
-        if(PlayerChoice ==="paper" || PlayerChoice ==="rock" || PlayerChoice ==="scissors"){
-            return PlayerChoice;
-        } else {
-            return "Unrecognized Input";
-        } }
+    //function playerSelection() {
+    //    const PlayerChoice = prompt("Choose rock, paper, or scissors").toLocaleLowerCase();
+    //    if(PlayerChoice ==="paper" || PlayerChoice ==="rock" || PlayerChoice ==="scissors"){
+    //        return PlayerChoice;
+     //   } else {
+    //        return "Unrecognized Input";
+    //    } }
         
     // Getting Computer Input
     function computerSelection() {
@@ -32,13 +32,16 @@
     }
    
     //Playing a Game of 5 Rounds
-    function game() {
-        let computerScore = 0;
-        let playerScore = 0;
+    //function game() {
+       // let computerScore = 0;
+       // let playerScore = 0;
 
-        for (i=0;i<5;i++) {
-
-            const result = playRound(playerSelection(), computerSelection());
+        //for (i=0;i<5;i++) {
+        const buttonRock = document.querySelector("#rock");
+        const buttonPaper = document.querySelector("#paper");
+        const buttonScissors = document.querySelector("#scissors");
+        buttonRock.addEventListener('click', () => {
+            const result = playRound("rock", computerSelection());
             console.log(result);
             if (result.includes("Win!")) {
                 playerScore++;
@@ -47,17 +50,43 @@
                 computerScore++;
                 console.log("Computer:" + computerScore + "|" + "Player:" + playerScore);
             }
-        }
-        console.log(
-            "Final Results: Player: " + playerScore + " | " + "Computer: " + computerScore
-        );
-        if (playerScore>computerScore) {
-            console.log("You Win the Game!");
-        } else if (playerScore<computerScore) {
-            console.log("You Lose the Game!");
-        } else {
-            console.log("The Game was a tie!");
-        }
-    }
+        });
+
+        buttonPaper.addEventListener('click', () => {
+            const result = playRound("paper", computerSelection());
+            console.log(result);
+            if (result.includes("Win!")) {
+                playerScore++;
+                console.log("Computer:" + computerScore + "|" + "Player:" + playerScore);
+            } else if (result.includes("Lose!")) {
+                computerScore++;
+                console.log("Computer:" + computerScore + "|" + "Player:" + playerScore);
+            }
+        });
+
+        buttonScissors.addEventListener('click', () => {
+            const result = playRound("scissors", computerSelection());
+            console.log(result);
+            if (result.includes("Win!")) {
+                playerScore++;
+                console.log("Computer:" + computerScore + "|" + "Player:" + playerScore);
+            } else if (result.includes("Lose!")) {
+                computerScore++;
+                console.log("Computer:" + computerScore + "|" + "Player:" + playerScore);
+            }
+        });
+    
+        
+//        console.log(
+//            "Final Results: Player: " + playerScore + " | " + "Computer: " + computerScore
+//        );
+//        if (playerScore>computerScore) {
+//            console.log("You Win the Game!");
+//        } else if (playerScore<computerScore) {
+//            console.log("You Lose the Game!");
+//        } else {
+//            console.log("The Game was a tie!");
+//        }
+    //}
    
-game();
+//game();
